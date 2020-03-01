8c02effc408834a5d1ba2d3e001056e4e282d906
AndreNunes1812 <andre.nunes0207700@gmail.com>
2020-03-01 23:32:09 +0100

    insert desafio4

---
+++ desafio4/index.js
@@ -0,0 +1,75 @@
+const user = {
+  name: "André Nunes",
+  transactions: [],
+  balance: 0
+};
+
+function createTransaction(type, value) {
+
+  return {type , value}
+
+}
+
+function addTransaction(user,transaction) {
+
+  user.transactions.push(transaction)
+
+}
+
+
+function getHigherTransactionByType(users, type) {
+  let maior = [];
+  for(let user of users ) {
+    if(user.type === type ) {
+      maior.push(user.value)
+    }
+  }
+  return {type, value: Math.max(...maior)};  
+}
+
+function getAverageTransactionValue(users) {
+  let media = 0;
+  for(let user of users ) {
+    media += user.value
+  }
+  return (media / users.length );  
+
+} 
+
+function getTransactionsCount(users) {
+  let tcredit =0, tdebit = 0;
+  for(let user of users ) {
+    if (user.type === 'credit') {
+      tcredit += 1
+    } else {
+      tdebit += 1
+    }    
+  }
+
+  return { 'credit': tcredit, 'debit': tdebit}
+
+}
+
+transaction = createTransaction('credit', 50);
+addTransaction(user, transaction )
+
+transaction = createTransaction('credit', 120);
+addTransaction(user, transaction )
+
+transaction = createTransaction('debit', 80);
+addTransaction(user, transaction )
+
+transaction = createTransaction('debit', 30);
+addTransaction(user, transaction )
+
+
+
+console.table(user);
+
+console.log(getHigherTransactionByType(user.transactions, 'credit'))
+
+console.log(getHigherTransactionByType(user.transactions, 'debit'))
+
+console.log(getAverageTransactionValue(user.transactions))
+
+console.log(getTransactionsCount(user.transactions ))
